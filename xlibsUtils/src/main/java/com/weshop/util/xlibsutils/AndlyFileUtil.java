4cae31aa09e7e7e1db6d0f6d2f96483f6500f827
luandly <luandly@users.noreply.github.com>
2022-09-13 17:12:52 +0800

    Update AndlyFileUtil.java
    复制文件夹及其中的文件

--- xlibsUtils/src/main/java/com/weshop/util/xlibsutils/AndlyFileUtil.java
+++ xlibsUtils/src/main/java/com/weshop/util/xlibsutils/AndlyFileUtil.java
@@ -67,6 +67,65 @@ public class AndlyFileUtil {
                 }
         }
     }
+    
+    /**
+     * 复制文件夹及其中的文件
+     *
+     * @param oldPath String 原文件夹路径 如：data/user/0/com.test/files
+     * @param newPath String 复制后的路径 如：data/user/0/com.test/cache
+     * @return <code>true</code> if and only if the directory and files were copied;
+     * <code>false</code> otherwise
+     */
+    public boolean copyFolder(String oldPath, String newPath) {
+        try {
+            File newFile = new File(newPath);
+            if (!newFile.exists()) {
+                if (!newFile.mkdirs()) {
+                    Log.d("--Method--", "copyFolder: cannot create directory.");
+                    return false;
+                }
+            }
+            File oldFile = new File(oldPath);
+            String[] files = oldFile.list();
+            File temp;
+            for (String file : files) {
+                if (oldPath.endsWith(File.separator)) {
+                    temp = new File(oldPath + file);
+                } else {
+                    temp = new File(oldPath + File.separator + file);
+                }
+
+                if (temp.isDirectory()) {   //如果是子文件夹
+                    copyFolder(oldPath + "/" + file, newPath + "/" + file);
+                } else if (!temp.exists()) {
+                    Log.d("--Method--", "copyFolder:  oldFile not exist.");
+                    return false;
+                } else if (!temp.isFile()) {
+                    Log.d("--Method--", "copyFolder:  oldFile not file.");
+                    return false;
+                } else if (!temp.canRead()) {
+                    Log.d("--Method--", "copyFolder:  oldFile cannot read.");
+                    return false;
+                } else {
+                    FileInputStream fileInputStream = new FileInputStream(temp);
+                    FileOutputStream fileOutputStream = new FileOutputStream(newPath + "/" + temp.getName());
+                    byte[] buffer = new byte[1024];
+                    int byteRead;
+                    //!=-1 也可以写成！=null,意思是读取的数据不为负数或者null就说明还没有读取完毕
+                    while ((byteRead = fileInputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, byteRead);
+                    }
+                    fileInputStream.close();
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     /**
      * 创建文件
